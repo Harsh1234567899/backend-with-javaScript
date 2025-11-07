11859f7489826178c86135701e6a4d31a43ed0e3
Harsh1234567899 <pankhaniyaharsh222@gmail.com>
2025-11-07 15:57:57 +0530

    make some change

---
+++ project/src/app.js
@@ -0,0 +1,12 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express()
+app.use(cors())
+
+app.use(express.json({limit: "100kb"}))
+app.use(express.urlencoded({extended: true,limit: "100kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+export { app }
--- project/src/index.js
+++ project/src/index.js
@@ -2,7 +2,7 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-// import { app } from './app.js'
+import { app } from './app.js'
 dotenv.config({
     path: './project/.env'
 })
@@ -10,14 +10,18 @@ dotenv.config({
 
 
 connectDB()
-// .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-//     })
-// })
-// .catch((err) => {
-//     console.log("MONGO db connection failed !!! ", err);
-// })
+.then(() => {
+    app.on("error", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+        })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
 
 
 
