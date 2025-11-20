e950252c121fa11583de65bf43153cb3693168d0
Harsh1234567899 <pankhaniyaharsh222@gmail.com>
2025-11-20 13:31:53 +0530

    add toggle, fetch subscribers, and subscribed channels logic; update route paths for clarity

--- project/src/app.js
+++ project/src/app.js
@@ -24,4 +24,7 @@ app.use("/api/v3/tweets", tweetsRouter)
 import playlistRouter from "./routes/playlist.routes.js"
 app.use("/api/v4/playlist", playlistRouter)
 
+import subscriptionRouter from "./routes/subscription.routes.js"
+app.use("/api/v5/subscription",subscriptionRouter)
+
 export { app }
--- project/src/controllers/subscription.controller.js
+++ project/src/controllers/subscription.controller.js
@@ -9,16 +9,120 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const toggleSubscription = asyncHandler(async (req, res) => {
     const {channelId} = req.params
     // TODO: toggle subscription
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "channel id is invalid")
+    }
+
+    const isSubscribed = await Subscription.findOne({ channel: channelId, subscriber: req.user._id });
+
+    if (req.user._id == channelId) {
+        throw new ApiError(400, "you subscribed your own channel")
+    }
+
+    if (!isSubscribed) {
+        const channel = await Subscription.create({
+            subscriber: req.user._id,
+            channel: channelId
+        })
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, channel, "Subscribed channel")
+            )
+
+    } else {
+        const channel = await isSubscribed.deleteOne();
+
+        return res
+            .status(200)
+            .json(
+                new ApiResponse(200, channel, "Unsubscribed channel")
+            )
+    }
 })
 
 // controller to return subscriber list of a channel
 const getUserChannelSubscribers = asyncHandler(async (req, res) => {
     const {channelId} = req.params
+    if (!isValidObjectId(channelId)) {
+        throw new ApiError(400, "channel id is invalid")
+    }
+    const getUserSubscribers = await Subscription.aggregate([
+        {
+            $match: {
+                channel: new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscribers",
+                foreignField: "_id",
+                as: "subscribersDetails"
+            }
+        },
+        {
+            $unwind: "$subscribersDetails"
+        },
+        {
+            $project: {
+                _id: 0,
+                avatar: "$subscribersDetails.avatar",
+                username: "$subscribersDetails.username",
+                fullname: "$subscribersDetails.fullname"
+            }
+        }
+    ])
+    if (!getUserSubscribers) {
+        throw new ApiError(301,"subscribers not found")
+    }
+    return res.status(200).json(200,getUserSubscribers,"subsribers fetched")
 })
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
     const { subscriberId } = req.params
+    if (!isValidObjectId(subscriberId)) {
+        throw new ApiError(400, "subscriber id is invalid")
+    }
+    const getUserSubscribedChannel = await Subscription.aggregate([
+        {
+            $match: {
+                subscriber: new mongoose.Types.ObjectId(subscriberId)
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "channel",
+                foreignField: "_id",
+                as: "channelDetails"
+            }
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "subscribers",
+                foreignField: "_id",
+                as: "subscribersDetails"
+            }
+        },
+        {
+            $addFields: "$subscribersDetails"
+        },
+        {
+            $unwind: "$channelDetails"
+        },
+        {
+            $project: {
+                username: "$channelDetails.username",
+                avatar: "channelDetails.avatar",
+                fullname: "channelDetails.fullname",
+                subscriberCount: 1
+            }
+        }
+    ])
 })
 
 export {
